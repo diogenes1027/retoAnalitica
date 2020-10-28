6b2218d6b55c45e171d82f66f819e1d407aa047e
diogenes1027 <73489549+diogenes1027@users.noreply.github.com>
2020-10-27 22:05:49 -0600

    Add files via upload

---
+++ ElArteDeLaAnalitica.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <b>Ejercicios con Python</b>
+# 
+# <ol>
+#     <li>
+# Escribir una función de Python que encuentre los números primos en los primeros N números naturales.
+#         <ul>
+#             <li>N es el argumento de la función.</li>
+#             <li>Llamar la función desde el código principal.</li>
+#         </ul>
+#     </li>
+#     <li>
+# Escribir una función de Python que encuentre el producto cruz en vectores de 3 componentes.
+#         <ul>
+#             <li>La función recibe dos listas (vectores).</li>
+#             <li>La función regresa un tercer vector (lista) con el resultado.</li>
+#             <li>Llamar a la función desde el código principal.</li>
+#         </ul>
+#     </li>
+#     <li>
+# Re-escribir el problema 2, pero ahora los vectores son leídos desde un archivo de texto.
+#     </li>
+# </ol>
+
+# In[ ]:
+
+
+def findPrimes(n):
+    primes = []
+    '''
+    Las tres primeras funciones las desarrolle en Visual Studio porque me resultaba más cómodo
+    '''
+    # Primes?
+    return primes
+
+
+# In[ ]:
+
+
+def crossProduct(a, b):
+    product = []
+    # 3D cross product?
+    return product
+
+
+# In[ ]:
+
+
+def crossProduct():
+    # Read a, b from file: vectors.txt
+    product = []
+    return product
+
+
+# In[2]:
+
+
+import pandas as pd
+dataset = pd.read_csv('covid19_tweets.csv')
+dataset.head()
+
+
+# In[3]:
+
+
+dataset.tail()
+
+
+# In[8]:
+
+
+dataset.describe()
+
+
+# In[9]:
+
+
+dataset.columns
+
+
+# In[ ]:
+
+
+
+
